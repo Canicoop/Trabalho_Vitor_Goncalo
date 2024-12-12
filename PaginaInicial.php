c3fbf45d81ea547fd478ff29dd8a617f5d20475c
Canicoop <94050980+Canicoop@users.noreply.github.com>
2024-12-12 11:40:51 +0000

    pagina inicial e login

---
+++ PaginaInicial.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navigation Menu</title>
+    <link rel="stylesheet" href="PaginaInicial.css">
+</head>
+<body>
+    <nav class="navbar">
+        <div class="nav-left">
+            <a href="#">Home</a>
+            <div class="dropdown">
+                <a href="#">Clothing</a>
+                <div class="dropdown-content">
+                    <a href="#">Option 1</a>
+                    <a href="#">Option 2</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <a href="#">Sneakers</a>
+                <div class="dropdown-content">
+                    <a href="#">Option 1</a>
+                    <a href="#">Option 2</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <a href="#">Clothing</a>
+                <div class="dropdown-content">
+                    <a href="#">Option 1</a>
+                    <a href="#">Option 2</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <a href="#">Accessories</a>
+                <div class="dropdown-content">
+                    <a href="#">Option 1</a>
+                    <a href="#">Option 2</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="nav-right">
+            <div class="icon">&#128269;</div>
+            <div class="icon"><a href="login.php">&#128100;</a></div>
+        </div>
+    </nav>
+</body>
+</html>
--- index.php
+++ index.php
@@ -12,6 +12,6 @@
     <div class="container">
         <h1>Bem-vindo à nossa Loja</h1>
         <p>Explore os melhores produtos e promoções exclusivas.</p>
-        <a href="login.php" class="btn">Entrar</a>
+        <a href="" class="btn">Entrar</a>
     </div>
 </body>
